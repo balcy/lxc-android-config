e421f04814b79b334d9f2b5a0430f14e20df4f35
Chris Clime <chris.clime@gmx.net>
2020-08-04 21:27:53 +0200

    Update disable-qtwebengine-gpu.sh

--- etc/profile.d/disable-qtwebengine-gpu.sh
+++ etc/profile.d/disable-qtwebengine-gpu.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 # Disable the GPU for QtWebEngine on Android-based devices
 # This file is part of lxc-android-config
+# QtWebEngine of Qt 5.11
+export QT_WEBENGINE_DISABLE_GPU=1
+# QtWebEngine of Qt 5.13 / 5.14
 export QTWEBENGINE_CHROMIUM_FLAGS="--disable-gpu --disable-viz-display-compositor"
